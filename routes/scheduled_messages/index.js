c5831073bb6af5c7f7426e41f834bd5cfd803e7b
Brycew73 <bwarner7@ycp.edu>
2023-10-23 19:39:55 -0400

    commit

--- routes/scheduled_messages/index.js
+++ routes/scheduled_messages/index.js
@@ -1,15 +1,41 @@
 'use strict'
+const mysqlx = require('@mysql/xdevapi');
 
 module.exports = async function (fastify, options) {
   fastify.get('/', async function (request, reply) {
     reply.code(200);
 
-    return { key: 'value' };
+    return { data: await listScheduledMessages() };
   });
+
 
   fastify.post('/', async function (request, reply) {
     reply.code(201);
 
     return { key: 'value' };
   });
+}
+
+async function listScheduledMessages() {
+  const config = process.env.MYSQLX_SCHEDULED_MESSAGES_DATABASE_URL;
+
+  let session;
+
+  try {
+    session = await mysqlx.getSession(config);
+
+    const result = await session.sql('SELECT 1;').execute();
+
+    const data = await result.fetchAll();
+
+    return data;
+  } catch (err) {
+    const { message, stack } = err;
+
+    console.error(JSON.stringify({ message, stack }));
+
+    process.exitCode = 1;
+  } finally {
+    await session?.close();
+  }
 }
