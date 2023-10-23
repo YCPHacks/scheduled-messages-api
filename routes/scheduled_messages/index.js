6eb579f10fa82092d15d6e615e3654f9ab4ab806
Ralph Greaves <rgreaves@ycp.edu>
2023-10-23 18:43:17 -0400

    Update index.js

--- routes/scheduled_messages/index.js
+++ routes/scheduled_messages/index.js
@@ -2,6 +2,14 @@
 
 module.exports = async function (fastify, options) {
   fastify.get('/', async function (request, reply) {
+    reply.code(200);
+
+    return { key: 'value' };
+  });
+
+  fastify.post('/', async function (request, reply) {
+    reply.code(201);
+
     return { key: 'value' };
   });
 }
